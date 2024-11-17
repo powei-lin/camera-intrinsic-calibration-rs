30fd574695cab3359a78c8e7310eafc7d15845b4
Powei Lin <poweilin1994@gmail.com>
2024-11-17 17:34:55 -0500

    trait

--- src/camera_model/generic.rs
+++ src/camera_model/generic.rs
@@ -46,43 +46,65 @@ pub fn remap(src: &DynamicImage, map0: &na::DMatrix<f32>, map1: &na::DMatrix<f32
     out
 }
 
-pub trait CameraModel {
-    fn project(&self, p3d: &[na::Point3<f64>]) -> Vec<Option<(f32, f32)>>;
-    fn unproject(&self, p2d: &[na::Point2<f64>]) -> Vec<Option<(f32, f32)>>;
-    fn init_undistort_map(
-        &self,
-        projection_mat: &na::Matrix3<f64>,
-        new_h_w: (u32, u32),
-    ) -> (na::DMatrix<f32>, na::DMatrix<f32>) {
-        if projection_mat.shape() != (3, 3) {
-            panic!("projection matrix has the wrong shape");
-        }
-        let fx = projection_mat[(0, 0)];
-        let fy = projection_mat[(1, 1)];
-        let cx = projection_mat[(0, 2)];
-        let cy = projection_mat[(1, 2)];
-        let p3ds: Vec<na::Point3<f64>> = (0..new_h_w.0)
-            .into_par_iter()
-            .flat_map(|y| {
-                (0..new_h_w.1)
-                    .into_par_iter()
-                    .map(|x| na::Point3::new((x as f64 - cx) / fx, (y as f64 - cy) / fy, 1.0))
-                    .collect::<Vec<na::Point3<f64>>>()
-            })
-            .collect();
-        let p2ds = self.project(&p3ds);
-        let (xvec, yvec): (Vec<f32>, Vec<f32>) = p2ds
-            .par_iter()
-            .map(|xy| {
-                if let Some((x, y)) = xy {
-                    (*x, *y)
+pub trait CameraModel<T: na::RealField + Clone>
+where
+    Self: Sync,
+{
+    fn params(&self) -> na::DVector<T>;
+    fn width(&self) -> T;
+    fn height(&self) -> T;
+    fn project_one(&self, pt: &na::Vector3<T>) -> na::Vector2<T>;
+    fn project(&self, p3d: &[na::Vector3<T>]) -> Vec<Option<na::Vector2<T>>> {
+        p3d.par_iter()
+            .map(|pt| {
+                let p2d = self.project_one(&pt);
+                if p2d[0] < T::from_f64(0.0).unwrap()
+                    || p2d[0] > self.width()
+                    || p2d[1] < T::from_f64(0.0).unwrap()
+                    || p2d[1] > self.height()
+                {
+                    None
                 } else {
-                    (f32::NAN, f32::NAN)
+                    Some(p2d)
                 }
             })
-            .unzip();
-        let xmap = na::DMatrix::from_vec(new_h_w.0 as usize, new_h_w.1 as usize, xvec);
-        let ymap = na::DMatrix::from_vec(new_h_w.0 as usize, new_h_w.1 as usize, yvec);
-        (xmap, ymap)
+            .collect()
     }
+}
+
+fn init_undistort_map(
+    camera_model: Box<&dyn CameraModel<f64>>,
+    projection_mat: &na::Matrix3<f64>,
+    new_h_w: (u32, u32),
+) -> (na::DMatrix<f32>, na::DMatrix<f32>) {
+    if projection_mat.shape() != (3, 3) {
+        panic!("projection matrix has the wrong shape");
+    }
+    let fx = projection_mat[(0, 0)];
+    let fy = projection_mat[(1, 1)];
+    let cx = projection_mat[(0, 2)];
+    let cy = projection_mat[(1, 2)];
+    let p3ds: Vec<na::Vector3<f64>> = (0..new_h_w.0)
+        .into_par_iter()
+        .flat_map(|y| {
+            (0..new_h_w.1)
+                .into_par_iter()
+                .map(|x| na::Vector3::new((x as f64 - cx) / fx, (y as f64 - cy) / fy, 1.0))
+                .collect::<Vec<na::Vector3<f64>>>()
+        })
+        .collect();
+    let p2ds = camera_model.project(&p3ds);
+    let (xvec, yvec): (Vec<f32>, Vec<f32>) = p2ds
+        .par_iter()
+        .map(|xy| {
+            if let Some(xy) = xy {
+                (xy[0] as f32, xy[1] as f32)
+            } else {
+                (f32::NAN, f32::NAN)
+            }
+        })
+        .unzip();
+    let xmap = na::DMatrix::from_vec(new_h_w.0 as usize, new_h_w.1 as usize, xvec);
+    let ymap = na::DMatrix::from_vec(new_h_w.0 as usize, new_h_w.1 as usize, yvec);
+    (xmap, ymap)
 }
--- src/camera_model/kb4.rs
+++ src/camera_model/kb4.rs
@@ -1,143 +1,159 @@
 use super::generic::CameraModel;
 use nalgebra as na;
+use num_traits::FromPrimitive;
 use rayon::prelude::*;
-use std::ops::Add;
 
-pub struct OpenCVFisheye {
-    pub fx: f64,
-    pub fy: f64,
-    pub cx: f64,
-    pub cy: f64,
-    pub k1: f64,
-    pub k2: f64,
-    pub k3: f64,
-    pub k4: f64,
+pub struct OpenCVFisheye<T: na::RealField + Clone> {
+    pub fx: T,
+    pub fy: T,
+    pub cx: T,
+    pub cy: T,
+    pub k1: T,
+    pub k2: T,
+    pub k3: T,
+    pub k4: T,
     pub width: u32,
     pub height: u32,
 }
-impl OpenCVFisheye {
+impl<T: na::RealField + Clone> OpenCVFisheye<T> {
     pub fn new(
-        camera_matrix: &na::Matrix3<f64>,
-        distortion: &na::DMatrix<f64>,
+        camera_matrix: &na::Matrix3<T>,
+        distortion: &na::DMatrix<T>,
         width: u32,
         height: u32,
-    ) -> OpenCVFisheye {
+    ) -> OpenCVFisheye<T> {
         OpenCVFisheye {
-            fx: camera_matrix[(0, 0)],
-            fy: camera_matrix[(1, 1)],
-            cx: camera_matrix[(0, 2)],
-            cy: camera_matrix[(1, 2)],
-            k1: distortion[0],
-            k2: distortion[1],
-            k3: distortion[2],
-            k4: distortion[3],
+            fx: camera_matrix[(0, 0)].clone(),
+            fy: camera_matrix[(1, 1)].clone(),
+            cx: camera_matrix[(0, 2)].clone(),
+            cy: camera_matrix[(1, 2)].clone(),
+            k1: distortion[0].clone(),
+            k2: distortion[1].clone(),
+            k3: distortion[2].clone(),
+            k4: distortion[3].clone(),
             width,
             height,
         }
     }
-    fn f(&self, theta: f64) -> f64 {
-        let theta2 = theta * theta;
-        let theta4 = theta2 * theta2;
-        let theta6 = theta2 * theta4;
-        let theta8 = theta2 * theta6;
-        theta * (1.0 + self.k1 * theta2 + self.k2 * theta4 + self.k3 * theta6 + self.k4 * theta8)
+    fn f(k1: &T, k2: &T, k3: &T, k4: &T, theta: &T) -> T {
+        let theta2 = theta.clone() * theta.clone();
+        let theta4 = theta2.clone() * theta2.clone();
+        let theta6 = theta2.clone() * theta4.clone();
+        let theta8 = theta2.clone() * theta6.clone();
+
+        theta.clone()
+            * (T::from_f64(1.0).unwrap()
+                + k1.clone() * theta2
+                + k2.clone() * theta4
+                + k3.clone() * theta6
+                + k4.clone() * theta8)
     }
-    fn df_dtheta(&self, theta: f64) -> f64 {
-        let theta2 = theta * theta;
-        let theta4 = theta2 * theta2;
-        let theta6 = theta2 * theta4;
-        let theta8 = theta2 * theta6;
-        1.0 + 3.0 * self.k1 * theta2
-            + 5.0 * self.k2 * theta4
-            + 7.0 * self.k3 * theta6
-            + 9.0 * self.k4 * theta8
+    fn df_dtheta(k1: &T, k2: &T, k3: &T, k4: &T, theta: T) -> T {
+        let theta2 = theta.clone() * theta;
+        let theta4 = theta2.clone() * theta2.clone();
+        let theta6 = theta2.clone() * theta4.clone();
+        let theta8 = theta2.clone() * theta6.clone();
+        T::from_f64(1.0).unwrap()
+            + T::from_f64(3.0).unwrap() * k1.clone() * theta2
+            + T::from_f64(5.0).unwrap() * k2.clone() * theta4
+            + T::from_f64(7.0).unwrap() * k3.clone() * theta6
+            + T::from_f64(9.0).unwrap() * k4.clone() * theta8
     }
-    fn project_one<T: na::RealField + Clone>(
-        params: &na::DVector<T>,
-        pt: &na::Vector3<T>,
-    ) -> Option<na::Vector2<T>> {
-        // let a = params[0] + params[1].clone();
-        // let b = a.clone() * a.clone();
+    fn project_one_impl(params: &na::DVector<T>, pt: &na::Vector3<T>) -> na::Vector2<T> {
         let xn = pt[0].clone() / pt[2].clone();
         let yn = pt[1].clone() / pt[2].clone();
-        xn.sin();
-        // let r2 = xn * xn + yn * yn;
-        // let r = r2.sqrt();
-        // let theta = r.atan();
-        // let theta_d = self.f(theta);
-        // let d = theta_d / r;
-        // let px = self.fx * (xn * d) + self.cx;
-        // let py = self.fy * (yn * d) + self.cy;
-        // if px < 0.0 || px > self.width as f64 || py < 0.0 || py > self.height as f64 {
-        //     None
-        // } else {
-        //     Some((px as f32, py as f32))
-        // }
-        // na::dvector![na::Vector2::new(a.clone(), a)]
-        None
+        let r2 = xn.clone() * xn.clone() + yn.clone() * yn.clone();
+        let r = r2.sqrt();
+        let theta = r.clone().atan();
+        let fx = &params[0];
+        let fy = &params[1];
+        let cx = &params[2];
+        let cy = &params[3];
+        let k1 = &params[4];
+        let k2 = &params[5];
+        let k3 = &params[6];
+        let k4 = &params[7];
+        let theta_d = Self::f(k1, k2, k3, k4, &theta);
+        let d = theta_d / r.clone();
+        let px = fx.clone() * (xn * d.clone()) + cx.clone();
+        let py = fy.clone() * (yn * d) + cy.clone();
+        na::Vector2::new(px, py)
     }
 }
-// num_dual::DualDVec64
+// // num_dual::DualDVec64
 
-impl CameraModel for OpenCVFisheye {
-    fn project(&self, p3d: &[nalgebra::Point3<f64>]) -> Vec<Option<(f32, f32)>> {
-        let param = na::dvector![3.0];
-        let p3d0 = na::Vector3::new(1.0, 2.0, 3.0);
-        // let param = na::dvector![num_dual::DualDVec64::from_re(0.0)];
-        // let p3d0 = na::Vector3::new(
-        //     num_dual::DualDVec64::from_re(0.0),
-        //     num_dual::DualDVec64::from_re(0.0),
-        //     num_dual::DualDVec64::from_re(0.0),
-        // );
-
-        Self::project_one(&param, &p3d0);
-        p3d.par_iter()
-            .map(|pt| {
-                let xn = pt.x / pt.z;
-                let yn: f64 = pt.y / pt.z;
-                let r2 = xn * xn + yn * yn;
-                let r = r2.sqrt();
-                let theta = r.atan();
-                let theta_d = self.f(theta);
-                let d = theta_d / r;
-                let px = self.fx * (xn * d) + self.cx;
-                let py = self.fy * (yn * d) + self.cy;
-                if px < 0.0 || px > self.width as f64 || py < 0.0 || py > self.height as f64 {
-                    None
-                } else {
-                    Some((px as f32, py as f32))
-                }
-            })
-            .collect()
+impl CameraModel<f64> for OpenCVFisheye<f64> {
+    fn params(&self) -> nalgebra::DVector<f64> {
+        na::dvector![self.fx, self.fy, self.cx, self.cy, self.k1, self.k2, self.k3, self.k4]
+    }
+    fn project_one(&self, pt: &nalgebra::Vector3<f64>) -> nalgebra::Vector2<f64> {
+        Self::project_one_impl(&self.params(), pt)
     }
 
-    fn unproject(&self, p2d: &[nalgebra::Point2<f64>]) -> Vec<Option<(f32, f32)>> {
-        p2d.into_par_iter()
-            .map(|p| {
-                if p.x < 0.0 || p.y < 0.0 || p.x >= self.width as f64 || p.y >= self.height as f64 {
-                    return None;
-                }
-                let xd = (p.x - self.cx) / self.fx;
-                let yd = (p.y - self.cy) / self.fy;
+    fn width(&self) -> f64 {
+        self.width as f64
+    }
 
-                let theta_d_2 = xd * xd + yd * yd;
-                let theta_d = theta_d_2.sqrt();
-                let mut theta = theta_d;
-                if theta > 1e-6 {
-                    for _ in 0..5 {
-                        let theta_next = theta - (self.f(theta) - theta_d) / self.df_dtheta(theta);
-                        if (theta_next - theta).abs() < 1e-6 {
-                            theta = theta_next;
-                            break;
-                        }
-                        theta = theta_next;
-                    }
-                    let scaling = theta.tan() / theta_d;
-                    Some(((xd * scaling) as f32, (yd * scaling) as f32))
-                } else {
-                    Some((0.0, 0.0))
-                }
-            })
-            .collect()
+    fn height(&self) -> f64 {
+        self.height as f64
+    }
+
+    // fn unproject(&self, p2d: &[nalgebra::Point2<f64>]) -> Vec<Option<(f32, f32)>> {
+    //     p2d.into_par_iter()
+    //         .map(|p| {
+    //             if p.x < 0.0 || p.y < 0.0 || p.x >= self.width as f64 || p.y >= self.height as f64 {
+    //                 return None;
+    //             }
+    //             let xd = (p.x - self.cx) / self.fx;
+    //             let yd = (p.y - self.cy) / self.fy;
+
+    //             let theta_d_2 = xd * xd + yd * yd;
+    //             let theta_d = theta_d_2.sqrt();
+    //             let mut theta = theta_d;
+    //             if theta > 1e-6 {
+    //                 for _ in 0..5 {
+    //                     let theta_next = theta - (self.f(theta) - theta_d) / self.df_dtheta(theta);
+    //                     if (theta_next - theta).abs() < 1e-6 {
+    //                         theta = theta_next;
+    //                         break;
+    //                     }
+    //                     theta = theta_next;
+    //                 }
+    //                 let scaling = theta.tan() / theta_d;
+    //                 Some(((xd * scaling) as f32, (yd * scaling) as f32))
+    //             } else {
+    //                 Some((0.0, 0.0))
+    //             }
+    //         })
+    //         .collect()
+    // }
+}
+
+impl CameraModel<num_dual::DualDVec64> for OpenCVFisheye<num_dual::DualDVec64> {
+    fn params(&self) -> nalgebra::DVector<num_dual::DualDVec64> {
+        na::dvector![
+            self.fx.clone(),
+            self.fy.clone(),
+            self.cx.clone(),
+            self.cy.clone(),
+            self.k1.clone(),
+            self.k2.clone(),
+            self.k3.clone(),
+            self.k4.clone()
+        ]
+    }
+    fn project_one(
+        &self,
+        pt: &nalgebra::Vector3<num_dual::DualDVec64>,
+    ) -> nalgebra::Vector2<num_dual::DualDVec64> {
+        Self::project_one_impl(&self.params(), pt)
+    }
+
+    fn width(&self) -> num_dual::DualDVec64 {
+        num_dual::DualDVec64::from_u32(self.width).unwrap()
+    }
+
+    fn height(&self) -> num_dual::DualDVec64 {
+        num_dual::DualDVec64::from_u32(self.height).unwrap()
     }
 }
