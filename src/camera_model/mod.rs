51dee0ff38e55a79d8e4da144dd5eac64913a545
Powei Lin <poweilin1994@gmail.com>
2024-11-22 18:32:23 -0500

    ucm

--- src/camera_model/mod.rs
+++ src/camera_model/mod.rs
@@ -2,6 +2,7 @@ pub mod eucm;
 pub mod generic;
 pub mod kb4;
 pub mod opencv5;
+pub mod ucm;
 
 pub use eucm::EUCM;
 pub use kb4::KannalaBrandt4;
---
+++ src/camera_model/ucm.rs
@@ -0,0 +1,100 @@
+use super::generic::CameraModel;
+use nalgebra as na;
+
+pub struct UCM<T: na::RealField + Clone> {
+    pub fx: T,
+    pub fy: T,
+    pub cx: T,
+    pub cy: T,
+    pub alpha: T,
+    pub width: u32,
+    pub height: u32,
+}
+impl<T: na::RealField + Clone> UCM<T> {
+    pub fn new(params: &na::DVector<T>, width: u32, height: u32) -> UCM<T> {
+        if params.shape() != (6, 1) {
+            panic!("the length of the vector should be 6");
+        }
+        UCM {
+            fx: params[0].clone(),
+            fy: params[1].clone(),
+            cx: params[2].clone(),
+            cy: params[3].clone(),
+            alpha: params[4].clone(),
+            width,
+            height,
+        }
+    }
+}
+
+impl<T: na::RealField + Clone> CameraModel<T> for UCM<T> {
+    #[inline]
+    fn params(&self) -> nalgebra::DVector<T> {
+        na::dvector![
+            self.fx.clone(),
+            self.fy.clone(),
+            self.cx.clone(),
+            self.cy.clone(),
+            self.alpha.clone(),
+        ]
+    }
+    fn width(&self) -> T {
+        T::from_u32(self.width).unwrap()
+    }
+
+    fn height(&self) -> T {
+        T::from_u32(self.height).unwrap()
+    }
+
+    fn project_one(&self, pt: &nalgebra::Vector3<T>) -> nalgebra::Vector2<T> {
+        let params = self.params();
+        let fx = &params[0];
+        let fy = &params[1];
+        let cx = &params[2];
+        let cy = &params[3];
+        let alpha = &params[4];
+
+        let x = pt[0].clone();
+        let y = pt[1].clone();
+        let z = pt[2].clone();
+
+        let r2 = x.clone() * x.clone() + y.clone() * y.clone();
+        let rho2 = r2.clone() + z.clone() * z.clone();
+        let rho = rho2.sqrt();
+
+        let norm = alpha.clone() * rho + (T::from_f64(1.0).unwrap() - alpha.clone()) * z;
+
+        let mx = x / norm.clone();
+        let my = y / norm.clone();
+
+        na::Vector2::new(fx.clone() * mx + cx.clone(), fy.clone() * my + cy.clone())
+    }
+
+    fn unproject_one(&self, pt: &nalgebra::Vector2<T>) -> nalgebra::Vector3<T> {
+        let params = self.params();
+        let fx = &params[0];
+        let fy = &params[1];
+        let cx = &params[2];
+        let cy = &params[3];
+        let alpha = &params[4];
+        let one = T::from_f64(1.0).unwrap();
+        let xi = alpha.clone() / (one.clone() - alpha.clone());
+
+        let mxx = (pt[0].clone() - cx.clone()) / fx.clone();
+        let myy = (pt[1].clone() - cy.clone()) / fy.clone();
+
+        let mx = (one.clone() - alpha.clone()) * mxx;
+        let my = (one.clone() - alpha.clone()) * myy;
+
+        let r2 = mx.clone() * mx.clone() + my.clone() * my.clone();
+
+        let xi2 = xi.clone() * xi.clone();
+        let n = (one.clone() + (one.clone() - xi2) * r2.clone()).sqrt();
+        let m = one.clone() + r2;
+
+        let k = (xi.clone() + n) / m;
+        let z = k.clone() - xi;
+
+        na::Vector3::new(k.clone() * mx / z.clone(), k * my / z, one)
+    }
+}
