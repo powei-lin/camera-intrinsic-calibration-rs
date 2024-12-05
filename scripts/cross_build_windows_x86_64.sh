b875f94e3f0c6e1976feea092b9b7dd551c7e235
Powei Lin <poweilin1994@gmail.com>
2024-12-04 23:18:25 -0500

    add windows

---
+++ scripts/cross_build_windows_x86_64.sh
@@ -0,0 +1,5 @@
+cargo zigbuild --release --target=x86_64-pc-windows-gnu
+cp ./target/x86_64-pc-windows-gnu/release/ccrs.exe ./ccrs.exe
+zip ccrs-x86_64-pc-windows-gnu.zip ./ccrs.exe
+shasum -a 256 ccrs-x86_64-pc-windows-gnu.zip > ccrs-x86_64-pc-windows-gnu.zip.sha256
+rm ./ccrs.exe
