88d54971da55ed6d3058799f29129757018b8fc9
Powei Lin <poweilin1994@gmail.com>
2025-03-16 14:29:38 -0400

    add python

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -3,3 +3,4 @@ cp ./target/release/ccrs ./ccrs
 tar czvf ccrs-aarch64-apple-darwin.tar.gz ./ccrs
 shasum -a 256 ccrs-aarch64-apple-darwin.tar.gz > ccrs-aarch64-apple-darwin.tar.gz.sha256
 rm ./ccrs
+maturin build -r
--- scripts/cross_build_linux_x86_64.sh
+++ scripts/cross_build_linux_x86_64.sh
@@ -3,3 +3,4 @@ cp ./target/x86_64-unknown-linux-gnu/release/ccrs ./ccrs
 tar czvf ccrs-x86_64-unknown-linux-gnu.tar.gz ./ccrs
 shasum -a 256 ccrs-x86_64-unknown-linux-gnu.tar.gz > ccrs-x86_64-unknown-linux-gnu.tar.gz.sha256
 rm ./ccrs
+maturin build -r --target x86_64-unknown-linux-gnu --zig
